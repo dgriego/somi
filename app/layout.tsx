b80d9dde1b22a365e7c4cfa1a5d62fffabc52ace
Daniel Griego <eggomygriego@gmail.com>
2026-09-08 23:00:33 -0700

    Use consolidated Somi styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "./enhancements.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://somi-lyart.vercel.app"),
